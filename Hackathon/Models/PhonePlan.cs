f15e3a1a04f5e3293052ce8f3aba1ad29e199395
dartrhevan <vovasatunkin@mail.ru>
2019-10-19 16:30:47 +0500

    plan actual version

--- Hackathon/Models/PhonePlan.cs
+++ Hackathon/Models/PhonePlan.cs
@@ -2,14 +2,89 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Hackathon.Models;
+
 namespace Hackathon.Models.PlanParameters
 {
     public class PhonePlan //Тариф
     {
-        PhonePlan()
-        {
+        /// <summary>
+        /// Исходящие звонки на номера МОТИВ
+        /// </summary>
+        private double OnMotiv;
 
+        /// <summary>
+        /// Исходящие звонки на номера региона подключения других операторов
+        /// </summary>
+        private double OnOtherOp;
+
+        /// <summary>
+        /// Исходящие звонки на номера региона подключения ГТС
+        /// </summary>
+        private double OnGTC;
+
+        /// <summary>
+        /// Исходящие звонки на номера РФ
+        /// </summary>
+        private double OnRF;
+
+        /// <summary>
+        /// Исходящие звонки международного соединения
+        /// </summary>
+        private double OnInternational;
+
+        /// <summary>
+        /// Мобильный интернет
+        /// </summary>
+        private double MobileInternet;
+
+        /// <summary>
+        /// Исходящие SMS-сообщения на номера РФ
+        /// </summary>
+        private double SmsOnRF;
+
+        /// <summary>
+        /// Исходящие SMS-сообщения на номера международных операторов
+        /// </summary>
+        private double SmsOnInternational;
+
+        /// <summary>
+        /// Исходящие MMS-сообщения на номера РФ
+        /// </summary>
+        private double MmsOnRF;
+
+        /// <summary>
+        /// Входящие в национальном роуминге
+        /// </summary>
+        private double IncomingNatRoaming;
+
+
+        /// <summary>
+        /// Тариф
+        /// </summary>
+        /// <param name="onMotiv">Исходящие звонки на номера МОТИВ</param>
+        /// <param name="onOtherOp">Исходящие звонки на номера региона подключения других операторов</param>
+        /// <param name="onGTC">Исходящие звонки на номера региона подключения ГТС</param>
+        /// <param name="onRF">Исходящие звонки на номера РФ</param>
+        /// <param name="onInternational">Исходящие звонки международного соединения</param>
+        /// <param name="mobileInternet">Мобильный интернет</param>
+        /// <param name="smsOnRF">Исходящие SMS-сообщения на номера РФ</param>
+        /// <param name="smsOnInternational">Исходящие SMS-сообщения на номера международных операторов</param>
+        /// <param name="mmsOnRF">Исходящие MMS-сообщения на номера РФ</param>
+        /// <param name="incomingNatRoaming">Входящие в национальном роуминге </param>
+        PhonePlan(double onMotiv, double onOtherOp, double onGTC, double onRF, double onInternational,
+        double mobileInternet, double smsOnRF, double smsOnInternational, double mmsOnRF,
+        double incomingNatRoaming)
+        {
+            OnMotiv = onMotiv;
+            OnOtherOp = onOtherOp;
+            OnGTC = onGTC;
+            OnRF = onRF;
+            OnInternational = onInternational;
+            MobileInternet = mobileInternet;
+            SmsOnRF = smsOnRF;
+            SmsOnInternational = smsOnInternational;
+            MmsOnRF = mmsOnRF;
+            IncomingNatRoaming = incomingNatRoaming;
         }
 
     }
