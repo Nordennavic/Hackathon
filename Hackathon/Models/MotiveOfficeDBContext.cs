09ad32f04fb4d973be071afb9a299522b7c374bf
dartrhevan <vovasatunkin@mail.ru>
2019-10-19 14:49:49 +0500

    some changes

--- Hackathon/Models/MotiveOfficeDBContext.cs
+++ Hackathon/Models/MotiveOfficeDBContext.cs
@@ -1,18 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Xml.Serialization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace Hackathon.Models
 {
@@ -28,7 +14,7 @@ namespace Hackathon.Models
 
         /*public DbSet<DBStar> Stars { get; set; }
         public DbSet<DBPlanet> Planets { get; set; }*/
-        public DbSet<DBUser> Users { get; set; }
+        //public DbSet<DBUser> Users { get; set; }
 
         public const string ConnectionString =
             "Server=tcp:hackatonmotiv.database.windows.net,1433;Initial Catalog=DB_MOTIV;Persist Security Info=False;User ID=Hackaton;Password=Team2019;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
