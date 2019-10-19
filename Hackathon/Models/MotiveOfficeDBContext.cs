0961ec255eb67c64b85a112d94aab0738f59434e
dartrhevan <vovasatunkin@mail.ru>
2019-10-19 13:55:48 +0500

    db_context first version

---
+++ Hackathon/Models/MotiveOfficeDBContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Hackathon.Models
+{
+    public class MotiveOfficeDBContext : DbContext
+    {
+        MotiveOfficeDBContext()
+        {
+            Database.EnsureCreated();
+        }
+        public MotiveOfficeDBContext(DbContextOptions<MotiveOfficeDBContext> options) : base(options)
+        {
+        }
+
+        /*public DbSet<DBStar> Stars { get; set; }
+        public DbSet<DBPlanet> Planets { get; set; }*/
+        public DbSet<DBUser> Users { get; set; }
+
+        public const string ConnectionString =
+            "Server=tcp:hackatonmotiv.database.windows.net,1433;Initial Catalog=DB_MOTIV;Persist Security Info=False;User ID=Hackaton;Password=Team2019;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+
+
+}
+
--- Hackathon/Startup.cs
+++ Hackathon/Startup.cs
@@ -8,6 +8,19 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Hackathon.Models;
 
 namespace Hackathon
 {
@@ -24,6 +37,20 @@ namespace Hackathon
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddSession();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+
+            services.AddDbContext<MotiveOfficeDBContext>(options =>
+                options.UseSqlServer(MotiveOfficeDBContext.ConnectionString)
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +72,8 @@ namespace Hackathon
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSession();
 
             app.UseEndpoints(endpoints =>
             {
